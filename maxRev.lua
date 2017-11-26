559f2dcec82270214a5d0554b6dd4e8dbeda5f7b
David Crocker <imdacrocker@gmail.com>
2017-11-26 07:14:52 -0800

    Changed tabs to double spaces

--- maxRev.lua
+++ maxRev.lua
@@ -4,9 +4,9 @@ maxRpmId = addChannel("MaxRpm",1,0,0,9000)
 maxRpm = 0
 
 function maxRev()
-	if rpm > maxRpm 
-		then
-		maxRpm = rpm
-		setChannel(maxRpmId, maxRpm)
-	end
+  if rpm > maxRpm 
+    then
+    maxRpm = rpm
+    setChannel(maxRpmId, maxRpm)
+  end
 end
--- shiftLight.lua
+++ shiftLight.lua
@@ -3,10 +3,10 @@
 timer = 0
 
 function shiftLight()
-	if rpm > 6250 then setGpio(1,1) else setGpio(1,0) end
-	if rpm > 6500 and timer == 1 --blinks shift light after set RPM
-		then
-			setGpio(1,0)
-	end
-	if timer == 1 then timer = 0 else timer = 1 end
+  if rpm > 6250 then setGpio(1,1) else setGpio(1,0) end
+  if rpm > 6500 and timer == 1 --blinks shift light after set RPM
+    then
+      setGpio(1,0)
+  end
+  if timer == 1 then timer = 0 else timer = 1 end
 end
