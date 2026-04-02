2903cfb0f3bad7fea8bc44ff764af016ed54f0a7
imdacrocker <32937272+imdacrocker@users.noreply.github.com>
2026-04-01 17:32:33 -0700

    Update UnicornLua.lua
    Updated to correct some logic, and shorten the script

--- Unicorn/UnicornLua.lua
+++ Unicorn/UnicornLua.lua
@@ -1,15 +1,14 @@
 local m = addChannel("MaxRpm", 1, 0, 0, 9000)
 local g = addChannel("Gear", 5, 0, 0, 5)
 local c = addChannel("Camera", 1, 0, 0, 2)
-stopTime = getUptime()
-moving = false
-engineOffTime = getUptime()
-running = false
-timer = 1
-lastResponse = getUptime()
-lastKeepAlive = getUptime()
-lastRequest = getUptime()
-lap = getLapCount()
+StopTime = getUptime()
+Moving = false
+EngineOffTime = getUptime()
+Timer = 1
+LastResponse = getUptime()
+LastKeepAlive = getUptime()
+LastRequest = getUptime()
+Lap = getLapCount()
 
 function sendRaw(val)
     for i = 1, #val do
@@ -57,7 +56,7 @@ function pi()
                 for i = 1, tonumber(length) do
                     packet = packet .. string.sub(readCSer(5, 100), 1, -3) .. ' '
                 end
-                lastResponse = getUptime()
+                LastResponse = getUptime()
                 if getChannel(c) == 0 then
                     setChannel(c, 1)
                 end
@@ -99,96 +98,73 @@ pi()
 
 setTickRate(10)
 function onTick()
-    -- checkMoving()
-    if (moving == false) then -- If the car was not moving last time we checked, check to see if it's moving
-        if ((getGpsSpeed() > 10) and (getChannel("RPM") > 500)) then -- If the car has begun to move while running, then do these things
-            moving = true -- Set Moving to true
+    local rpm = getChannel("RPM")
+
+    -- Various functions around stopping and going
+    if (Moving) then -- If the car was moving last time we checked
+        StopTime = getUptime() -- Always update when moving 
+        if (getGpsSpeed() == 0 and rpm == 0) then Moving = false end 
+    else -- if NOT moving
+        if ((getGpsSpeed() > 10) and (rpm > 500)) then -- If the car was not moving last time we checked, but now it is...
+            Moving = true
             sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x53, 0x48, 0x02),'1') -- Start GoPro
-            startLogging() -- Make sure logging is running
-            if ((getUptime() - stopTime) > 120000) then -- If the car has been stopped for 2 minutes
-                resetLapStats() -- Reset the lap stats
-                maxRPM = getChannel("RPM") -- Reset the maxRpm channel
-            end
-  end
-        if ((getUptime() - stopTime) > 30000) and isLogging() ~= 0 then -- If the car is stopped and logging, check to see if it's been stopped for 30 seconds
-   sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x53, 0x48,0x00), '1') -- Stop GoPro
-   stopLogging() -- Stop logging if the car has been stopped for 30s or more
-  end
-    end
-    if (moving) then -- If the car was moving last time we checked, check to see if it's stopped
-        stopTime = getUptime() -- Set the stop time to the current uptime constantly
-        if (getGpsSpeed() == 0 and getChannel("RPM") == 0) then -- Use getGpsSpeed() AND getChannel("RPM") for this
-            moving = false -- Set the car  to not moving
+            startLogging()
+        -- Stop logging if the car has been stopped for 30s
+        elseif ((getUptime() - StopTime) > 30000) and isLogging() ~= 0 then stopLogging()
+        -- Check to see if the car has been stopped for 2 minutes
+        elseif ((getUptime() - StopTime) > 120000) then
+            resetLapStats()
+            setChannel(m, rpm) -- Reset the maxRpm channel
         end
     end
-    -- checkRunning()
-    if (running == false) then
-        if (getChannel("RPM") > 500) then
-            running = true
-        end
-    end
-    if (running) then
-        engineOffTime = getUptime()
-        if (getChannel("RPM") == 0) then -- If the car is not running
-            running = false
-        end
+    if (rpm > 500) then
+        EngineOffTime = getUptime()
     end
 
-    -- maxRev
-    if getChannel("RPM") > getChannel("MaxRpm") then
-        setChannel(m, getChannel("RPM"))
-        if (moving) then
-            if (getChannel("RPM") > 6500) then
-            end
-        end
-    end
-    -- resetButton()
+    -- Max RPM
+    if rpm > getChannel(m) then setChannel(m, rpm) end -- Update max RPM if greater than current
+
+    -- Reset Button
     if getButton() == true then -- Checks the button on the front of the RaceCapture for a press
-        maxRpm = 0 -- Resets maximum getChannel("RPM") counter
-        setChannel(m, 0)
+        setChannel(m, rpm) -- Reset the MaxRpm
         resetLapStats()
-  stopTime = getUptime()
+        StopTime = getUptime()
     end
-    -- checkGear()
-    --local gearPos = calcGear(58.42, 3.73, 3.83, 2.2, 1.4, 1.0, .89)
- local gearPos = calcGear(58.42, 3.73, 4.2, 2.49, 1.66, 1.24, 1.0)
-    if (gearPos == nil) then
-        gearPos = 0 -- Set the gear to 0 if none is detected
-    end
-    setChannel(g, gearPos) -- Actually set the channel
 
-    -- checkOil()
-    if (getChannel("OilPress") < 5 and getChannel("RPM") <= 3000) or -- If the pressure is less than 3, and the getChannel("RPM") is less than 3000
-        (getChannel("RPM") > 3000 and getChannel("OilPress") < 20) -- If the getChannel("RPM") is above 3000, and the oil pressure is below 20
-    then
-        if (moving) then -- If the car is moving, set the light
-            setGpio(0, 1)
+    -- Gear Calculator
+    local gearPos = calcGear(58.42, 3.73, 4.2, 2.49, 1.66, 1.24, 1.0)
+    if (gearPos == nil) then gearPos = 0 end -- Set the gear to 0 if none is detected
+    setChannel(g, gearPos)
+
+    -- Oil Pressure
+    if (rpm > 3000 and getChannel("OilPress") < 20) then setGpio(0, 1)
+    elseif (getChannel("OilPress") < 5 and rpm <= 3000) then
+        if (Moving) then setGpio(0, 1) -- If the car is moving, set the light
         else
-            if ((getUptime() - engineOffTime) < 5000) then -- If the car is NOT moving, only show the light if the car has been stopped for less than 5 seconds
-                setGpio(0, 1)
+            if ((getUptime() - EngineOffTime) < 5000) then
+                setGpio(0, 1) -- If the car is NOT moving, only show the light if the car has been stopped for less than 5 seconds
             else
-                setGpio(0, 0) -- If the car has been stopped for more than 5 seconds, shut off the light
-            end
+                setGpio(0, 0) end -- If the car has been stopped for more than 5 seconds, shut off the light
         end
-    else
-        setGpio(0, 0) -- Otherwise, set the light to off
-    end
-    -- shiftLight()
-    if getChannel("RPM") > 5750 then
+    else setGpio(0, 0) end -- Otherwise, set the light to off
+
+    -- Shift Light
+    if rpm > 5750 then
         setGpio(1, 1)
     else
         setGpio(1, 0)
+        Timer = 0
     end
-    if getChannel("RPM") > 6000 then
-        if timer == 1 then
+    if rpm > 6000 then
+        if Timer == 1 then
             setGpio(1, 0)
-            timer = 0
+            Timer = 0
         else
-            timer = timer + 1
+            Timer = 1
         end
     end
-    -- checkCamera()
-    if (getUptime() - lastResponse) > 5000 then
+    -- Check Cameras
+    if (getUptime() - LastResponse) > 5000 then -- Send the WOL packet
         setChannel(c, 0)
         local mac = string.char(0xd4, 0xd9, 0x19, 0x99, 0xc4, 0xef)
         local packet = string.char(0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF)
@@ -197,32 +173,35 @@ function onTick()
         end
         sendCommand(packet, '0')
     end
-    if isLogging() == 0 and getChannel(c) == 2 then
-     sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x53, 0x48, 0x00), '1') -- Stop camera
+    if isLogging() == 0 and getChannel(c) == 2 then -- If we are not logging, but the camera is running, stop it
+        sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x53, 0x48, 0x00), '1')
         setChannel(c, 1)
     end
-    if isLogging() ~= 0 and getChannel(c) == 1 then -- If we ARE logging, and not recording, then record
-        sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x53, 0x48, 0x02), '1') -- Start camera
+    if isLogging() ~= 0 and getChannel(c) == 1 then -- If we ARE logging, and not recording, then start
+        sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x53, 0x48, 0x02), '1')
         setChannel(c, 2)
     end
-    if (getUptime() - lastKeepAlive) > 1500 then
+    -- Send KeepAlive every 1.5s
+    if (getUptime() - LastKeepAlive) > 1500 then
         sendAt('AT+CIPSEND=1,22')
         pi()
         sendRaw('_GPHD_:0:0:2:0.000000\n')
         pi()
-        lastKeepAlive = getUptime()
+        LastKeepAlive = getUptime()
     end
-    if (getUptime() - lastRequest) > 5000 then
+    -- Request an update every 5 seconds
+    if (getUptime() - LastRequest) > 5000 then
         sendCommand(string.char(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x73, 0x74), '1')
-        lastRequest = getUptime()
+        LastRequest = getUptime()
     end
-    -- Set tag
-    if lap ~= getLapCount() then
-        lap = getLapCount()
+    -- Set tag on lap rollover
+    if Lap ~= getLapCount() then
+        Lap = getLapCount()
         sendAt('AT+CIPSTART=3,"TCP","10.71.79.2",80')
         pi()
-        local get = 'GET /gp/gpControl/command/storage/tag_moment HTTP/1.0' .. string.char(13) .. string.char(10) .. string.char(13) .. string.char(10)
-        sendAt('AT+CIPSEND=3,' .. string.sub(#get,1, -3))
+        local get = 'GET /gp/gpControl/command/storage/tag_moment HTTP/1.0' ..
+            string.char(13) .. string.char(10) .. string.char(13) .. string.char(10)
+        sendAt('AT+CIPSEND=3,' .. string.sub(#get, 1, -3))
         pi()
         sendRaw(get)
         pi()
